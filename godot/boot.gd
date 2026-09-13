e571c3773af666e7030de0523b5caa65c1fba864
animemadness <jillninagarcia01@gmail.com>
2026-09-13 10:18:35 +0800

    Add files via upload

---
+++ godot/boot.gd
@@ -0,0 +1,39 @@
+extends Node
+
+const SERVER_SCENE = preload("res://server/server_main.tscn")
+const CLIENT_SCENE = preload("res://client/client_main.tscn")
+
+func _ready() -> void:
+	var mode: GameOrchestrator.ExecutionMode = GameOrchestrator.ExecutionMode.STANDALONE_CLIENT
+	var args: PackedStringArray = OS.get_cmdline_args()
+
+	for arg: String in args:
+		match arg:
+			"--server":
+				mode = GameOrchestrator.ExecutionMode.DEDICATED_SERVER
+			"--listen-host":
+				mode = GameOrchestrator.ExecutionMode.LISTEN_HOST
+			"--client":
+				mode = GameOrchestrator.ExecutionMode.STANDALONE_CLIENT
+
+	if mode == GameOrchestrator.ExecutionMode.STANDALONE_CLIENT:
+		if OS.has_feature("dedicated_server") or DisplayServer.get_name() == "headless":
+			mode = GameOrchestrator.ExecutionMode.DEDICATED_SERVER
+
+	GameOrchestrator.initialize_mode(mode)
+
+	if mode == GameOrchestrator.ExecutionMode.DEDICATED_SERVER:
+		_boot_server()
+	else:
+		_boot_client()
+
+func _boot_server() -> void:
+	print("[BOOT] Dedicated Server flag detected.")
+	var server_instance := SERVER_SCENE.instantiate() as ServerMain
+	get_tree().root.add_child.call_deferred(server_instance)
+	queue_free()
+
+func _boot_client() -> void:
+	print("[BOOT] Client environment detected.")
+	SceneManager.transition_to_screen(Scenes.Menus.TITLE_SCREEN)
+	queue_free()
